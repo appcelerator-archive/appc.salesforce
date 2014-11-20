7dd01c86ba1375460df9801c09d9ecd3116f0931
Dawson Toth <dtoth@appcelerator.com>
2014-11-20 14:19:13 -0500

    [API-157] Support Generating Salesforce Conn
    https://jira.appcelerator.org/browse/API-157

---
+++ conf/local.example.js
@@ -0,0 +1,7 @@
+module.exports = {
+	requireSessionLogin: false,
+	url: 'https://test.salesforce.com/',
+	username: 'your_salesforce_username',
+	password: 'your_salesforce_password',
+	token: 'your_salesforce_token'
+};

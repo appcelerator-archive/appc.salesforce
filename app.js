2357a1a10a2f543143862efbac666c46e8c7ed60
Dawson Toth <dtoth@appcelerator.com>
2014-12-16 13:49:27 -0500

    https://jira.appcelerator.org/browse/API-230

--- app.js
+++ app.js
@@ -1,45 +1,12 @@
 /**
- * NOTE: this file is simply for testing your Connector and
- * not used or packaged with the actual connector when published
+ * NOTE: This file is simply for testing this connector and will not
+ * be used or packaged with the actual connector when published.
  */
 var APIBuilder = require('appcelerator').apibuilder,
 	server = new APIBuilder(),
 	connector = server.getConnector('appc.salesforce');
 
-// lifecycle examples
-server.on('starting', function(){
-	server.logger.info('server is starting!');
-});
-
-server.on('started', function(){
-	server.logger.info('server started!');
-});
-
-//--------------------- implement authorization ---------------------//
-
-// fetch our configured apikey
-var apikey = server.get('apikey');
-server.logger.info('APIKey is:',apikey);
-
-function APIKeyAuthorization(req, resp, next) {
-	if (!apikey) return next();
-	if (req.headers['apikey']) {
-		var key = req.headers['apikey'];
-		if (key == apikey) {
-			return next();
-		}
-	}
-	resp.status(401);
-	return resp.json({
-		id: "com.appcelerator.api.unauthorized",
-		message: "Unauthorized",
-		url: ""
-	});
-}
-
-//--------------------- simple user model ---------------------//
-
-var Account = APIBuilder.Model.extend('Account',{
+var Account = APIBuilder.Model.extend('Account', {
 	fields: {
 		Name: { type: String, required: false, validator: /[a-zA-Z]{3,}/ },
 		Type: { type: String, readonly: true },
@@ -47,20 +14,15 @@ var Account = APIBuilder.Model.extend('Account',{
 	},
 	connector: 'appc.salesforce'
 });
-
-// create a user api from a user model
 server.addModel(Account);
 
-// add an authorization policy for all requests at the server log
-server.authorization = APIKeyAuthorization;
-
-// start the server
 server.start(function(err) {
 	if (err) {
 		return server.logger.fatal(err);
 	}
 	server.logger.info('server started on port', server.port);
-	connector.fetchSchema(function(err,schema){
+
+	connector.fetchSchema(function(err, schema) {
 		server.logger.info('server fetched schema');
 		err && server.logger.error(err);
 	});

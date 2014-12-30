23f30c56d038d3c2603c5d790ce509ed5d932eb9
Dawson Toth <dtoth@appcelerator.com>
2014-12-30 15:40:34 -0500

    https://jira.appcelerator.org/browse/API-263

---
+++ conf/example.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	connectors: {
+		'appc.salesforce': {
+			/*
+			 requireSessionLogin allows you to configure whether or not anonymous requests can use your default
+			 account (specified below) for connecting to Salesforce. Set this to "true" to require requests to specify
+			 their own credentials or their own session token (via the headers user, pass, and token, or accessToken).
+			 */
+			requireSessionLogin: false,
+			url: 'https://test.salesforce.com/',
+			username: 'your_salesforce_username',
+			password: 'your_salesforce_password',
+			token: 'your_salesforce_token'
+		}
+	}
+};
--- conf/local.example.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	requireSessionLogin: false,
-	url: 'https://test.salesforce.com/',
-	username: 'your_salesforce_username',
-	password: 'your_salesforce_password',
-	token: 'your_salesforce_token'
-};

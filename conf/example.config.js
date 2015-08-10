d9687b812ae53a9312180b74d4ac20650f897ec1
Dawson Toth <dtoth@appcelerator.com>
2015-08-10 15:18:03 -0400

    [API-872] Document schemaRefresh
    https://jira.appcelerator.org/browse/API-872

--- conf/example.config.js
+++ conf/example.config.js
@@ -7,11 +7,21 @@ module.exports = {
 			 their own credentials or their own session token (via the headers user, pass, and token, or accessToken).
 			 */
 			requireSessionLogin: false,
+
+			/*
+			 Configure how to connect to your instance; this will be used for looking up metadata and,
+			 if requireSessionLogin is false, for executing methods.
+			 */
 			url: 'https://test.salesforce.com/',
 			username: 'your_salesforce_username',
 			password: 'your_salesforce_password',
 			token: 'your_salesforce_token',
 
+			/*
+			 Other settings.
+			 */
+			schemaRefresh: 3.6e+6 * 24, // how long the cache will live (one day); set to "false" to disable caching.
+			modelAutogen: false, // whether or not to generate APIs for the built-in Salesforce models.
 			generateModelsFromSchema: true // Generate models from your schema.
 		}
 	}
